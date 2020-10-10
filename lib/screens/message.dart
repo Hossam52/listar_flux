09a1161a7ebe0eb43361584b6f92a06e0b484d58
Dell <mohamedhany2000>
2020-10-10 15:52:06 +0200

    finally

--- lib/screens/message.dart
+++ lib/screens/message.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class Messages extends StatelessWidget{
+class Messages extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold( appBar:AppBar(
-       centerTitle:true,
-       title: Text("Messages"),
-     ),
-     body:Center(child:Text("Messages"))
-   );
+    return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Messages"),
+        ),
+        body: Center(child: Text("messages")));
   }
 }
